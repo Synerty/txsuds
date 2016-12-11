7210ff35c2f950f1a59be9c316910362a3979966
Jarrod Chesney <jarrod.chesney@synerty.com>
2016-12-11 17:28:57 +1000

    Tweaked some build code

--- pipbuild.sh
+++ pipbuild.sh
@@ -25,11 +25,8 @@ fi
 
 echo "Setting version to $VER"
 
-# Update the setup.py
-sed -i "s;^package_version.*=.*;package_version = '${VER}';"  setup.py
-
 # Update the package version
-sed -i "s;.*version.*;__version__ = '${VER}';" ${PACKAGE}/__init__.py
+sed -i "s;.*version.*;__version__ = '${VER}';" suds/__init__.py
 
 # Upload to test pypi
 python setup.py sdist upload -r pypitest
